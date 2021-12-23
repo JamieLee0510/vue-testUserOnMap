5926a35c4a7e472723e504012ad6a14aa7e7955c
Ding Yu Lee <dingyu@logflows.com>
2021-12-23 14:19:17 +0800

    ADD:vue-mapbox component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="home">
     <p>first commit</p>
+    <div style="height: 500px">
+      <MglMap :accessToken="accessToken" :mapStyle="mapStyle" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
 
 export default {
   name: "Home",
-  components: {},
+  components: { MglMap },
+  data() {
+    return {
+      // your access token. Needed if you using Mapbox maps
+      accessToken:
+        "pk.eyJ1IjoiamFtaWUwNTEwIiwiYSI6ImNreGlqZ295MjBsN3AzMXF3YnhqYmpndmsifQ.sw_aNvmT2e2vN9TCBGayQg",
+      // your map style
+      mapStyle: "mapbox://styles/jamie0510/ckxijwvzj0mad15n4ckyxh580",
+    };
+  },
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
+  },
 };
 </script>
